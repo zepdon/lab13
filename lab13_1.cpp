210f0ef6592d2005e352316f3aef8c802bfb5435
Poon <zeepengineer@gmail.com>
2024-01-05 13:38:21 +0700

    lab13_1 completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,39 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[], int N, double B[]) {
+    int i = 0;
+
+    while (i < 6) {
+        B[i] = 0;
+        i++;
+    }
+
+    B[1] = 0, B[2] = 1, B[5] = A[0];
+    i = 0;
+
+    while (i < N) {
+        B[0] += A[i];
+        B[2] *= A[i];
+        B[3] += (1 / A[i]);
+        
+        if (A[i] > B[4]) B[4] = A[i];
+        if (A[i] < B[5]) B[5] = A[i];
+        
+        i++;
+    }
+
+    B[0] /= N;
+
+    i = 0;
+
+    while (i < N) {
+        B[1] += pow(A[i] - B[0], 2);
+        i++;
+    }
+
+    B[1] = sqrt(B[1] / N);
+    B[2] = pow(B[2], (1.0 / N));
+    B[3] = N / B[3];
+}
