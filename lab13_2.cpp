2bfb5af133162b2cd046af2e018a6d72d1a78767
Poon <zeepengineer@gmail.com>
2024-01-05 13:50:00 +0700

    lab13_2 completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,48 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool image[N][M]) {
+    int i = 0;
+    while (i < M + 2) {
+        std::cout << "-";
+        i++;
+    }
+    std::cout << "\n";
+
+    int y = 0;
+    while (y < N) {
+        std::cout << "|";
+        int x = 0;
+        while (x < M) {
+            if (image[y][x] == 1)
+                std::cout << "*";
+            else
+                std::cout << " ";
+            x++;
+        }
+        std::cout << "|\n";
+        y++;
+    }
+
+    i = 0;
+    while (i < M + 2) {
+        std::cout << "-";
+        i++;
+    }
+    std::cout << "\n";
+}
+
+void updateImage(bool image[N][M], int s, int x, int y) {
+    int j = 0;
+    while (j < M) {
+        int i = 0;
+        while (i < N) {
+            if (sqrt(pow(i - x, 2) + pow(j - y, 2)) <= s - 1) {
+                image[i][j] = 1;
+            }
+            i++;
+        }
+        j++;
+    }
+}
